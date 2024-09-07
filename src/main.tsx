73a3160dfbef36cbaa008c535b8235884c023a1e
vietanh <vietanhle2662001@gmail.com>
2024-09-07 10:25:40 +0700

    install axios dot env react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,23 @@
 import { CssBaseline } from "@mui/material";
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "@emotion/react";
 import theme from "~/theme";
 import { store } from "~/redux/store";
 import App from "~/App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <App />
       </ThemeProvider>
     </Provider>
-  </React.StrictMode>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
